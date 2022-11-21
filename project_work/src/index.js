32eea5a40a5b4d207d4e9b452a8ec32bab445adf
tg732 <108361501+tg732@users.noreply.github.com>
2022-11-21 22:22:37 +0500

    Добавлены уведомления при создании задания/документа.

--- project_work/src/index.js
+++ project_work/src/index.js
@@ -4,6 +4,7 @@ const app = express();
 import session from 'express-session'
 import path from 'path'
 import flash from 'connect-flash'
+import { WebSocketServer } from 'ws';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -47,3 +48,35 @@ import {register as task} from './entities/task/index.js'
     console.log('Server started on 8080 port')
   })
 })()
+
+const wss = new WebSocketServer({noServer: true});
+
+app.get('/ws', (req, res) => {
+  if (req.headers.upgrade && req.headers.upgrade.toLowerCase() == 'websocket' &&
+      // может быть подключён: keep-alive, Upgrade
+      req.headers.connection.match(/\bupgrade\b/i)) 
+  {
+      wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+  }
+});
+
+function onSocketConnect(ws) {
+  console.log(`новое подключение`);
+
+  ws.on('message', function(data, isBinary) {
+    let message = isBinary ? data : data.toString();
+    console.log(`получено сообщение:`);
+    console.log(message);
+    let messageObj = JSON.parse(message)
+    
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === ws.OPEN) {
+        client.send(`${messageObj.message} номер ${messageObj.id}`);
+      }
+    });
+  });
+
+  ws.on('close', function() {
+    console.log(`подключение закрыто`);
+  });
+}
